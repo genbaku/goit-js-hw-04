d1096c124902522ab6a2265759f15eb9cf282e45
genbaku <140975610+genbaku@users.noreply.github.com>
2023-11-04 03:38:45 +0100

    1231

--- task-3.js
+++ task-3.js
@@ -1,11 +1,19 @@
-;`use strict`
-function getElementWidth(content, padding, border) {
-  return (
-    parseFloat(content) +
-    parseFloat(padding) * 2 +
-    parseFloat(border) * 2
-  )
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+`use strict`
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername: function(newName) {
+      this.username = newName;
+  },
+  updatePlayTime: function(hours) {
+      this.playTime += hours;
+  },
+  getInfo: function() {
+      return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
